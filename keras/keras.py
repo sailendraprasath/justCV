07e4dfb7aa3fcc9e626a19645e5d1dc370836acb
sailendraprasath <sailendraprasath05@gmail.com>
2025-04-28 22:16:10 +0530

    well

--- keras/keras.py
+++ keras/keras.py
@@ -2,9 +2,14 @@ import numpy as np
 from numpy import genfromtxt
 
 data = genfromtxt('data_banknote_authentication.txt', delimiter=',')
-print(data)
+
 
 labels = data[:,4]
-print(labels)
 features = data[:,0:4]
+
+
+x = features
+y = labels
+
 print(features)
+print(labels)
