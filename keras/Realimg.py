210ba85ff2a73e5f5a9838855b329c2743384367
sailendraprasath <sailendraprasath05@gmail.com>
2025-05-03 22:41:11 +0530

    well

--- keras/Realimg.py
+++ keras/Realimg.py
@@ -28,7 +28,7 @@ transformed_dog = image_gen.random_transform(dog)
 image_gen.flow_from_directory('CATS_DOGS/train')
 
 # Show the transformed image
-plt.imshow(transformed_dog)
-plt.axis('off')
-plt.title("Augmented Image")
-plt.show()
+# plt.imshow(transformed_dog)
+# plt.axis('off')
+# plt.title("Augmented Image")
+# plt.show()
