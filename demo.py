84b2c69e3ad27061f5a6799255c65a9d0d322369
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-27 21:45:36 +0530

    well

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
-sailesh = "Become a Data Scientist"
+class Student:
+    def __init__(self,name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+    
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.grade)
+
+stud = Student("Sailesh",20,7.2)
+stud.display()
